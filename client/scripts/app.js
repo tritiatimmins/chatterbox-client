6d9cf8193d5d6c2f356b1fd3fc3b8df7d636035f
tritiatimmins <tritialtimmins@gmail.com>
2016-02-05 17:29:49 -0800

    able to pass all tests except for last two event tests

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -25,29 +25,39 @@ app.send = function(){
 
   $.ajax({
     type: "POST",
-    url: "https://api.parse.com/1/classes/chatterbox.jsonp",
+    url: "https://api.parse.com/1/classes/chatterbox",
     data: JSON.stringify(message),
     success: function(){}
-    // error: function(){}
+    
   })
 };
+
 
 
 app.fetch = function(){
   // return true;
   $.ajax({
     type: "GET",
+
     url: undefined,
     success: function(){}
     // error: function(){}
   })
 };
+// var ajaxInfo = $.ajax({
+  // type: "GET",
+  // url: "https://api.parse.com/1/classes/chatterbox",
+  // data: jsonp
+// })
 
 
 app.clearMessages = function(){
   //how to get rid of #chats
+  //right now we have to put something into chats
+  //first we have to make chats
+  //pull things after chats
+  //and get chats
   $("#chats").html('');
-
 };
 
 app.addMessage = function(){
@@ -57,12 +67,21 @@ app.addMessage = function(){
   roomname: 'lobby'
   };
   var $message = $(message);
-  $('#chats').append($message);
-};
+  $message = ("<div>" + message +"</div>")
 
-app.addRoom = function(string){
+  $('#chats').append($message);
   
 };
 
+app.addRoom = function(message){
+  var $message = $(message);
+  $message = ("<div>" + message + "</div>");
+  $('#roomSelect').append($message);
+};
 
+app.addFriend = function(){
+ $("#addFriend").on('click', function(){
+    console.log("hello friends");
+ })
+};
 
